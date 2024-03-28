84f91e154570e34509f19eb122c68478b9aa15a6
BeaCp123 <beatrizcustodio262@gmail.com>
2024-03-28 15:06:36 +0100

    feature(client):Fixing-image-responsive

--- src/components/Parallax.jsx
+++ src/components/Parallax.jsx
@@ -4,7 +4,7 @@ const Parallax = () => {
   return (
     <div className='flex items-center justify-center h-80 bg-fixed bg-Japon bg-cover relative'>
       <h1 className='text-4xl md:text-8xl text-white uppercase relative z-10 bg-blue-500 bg-opacity-70 p-4 md:p-6 rounded border border-blue-700 shadow-lg text-center'>
-        Reserva y disfruta
+        Reserva y disfruta.
       </h1>
     </div>
   );
